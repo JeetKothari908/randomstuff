48071922c1f8f17a93ce4b0ebc5ec625683e0bdd
Jeet Kothari <jkothari@cps.edu>
2022-06-13 02:00:01 -0500

    making javascripter

---
+++ javascripter.js
@@ -0,0 +1,15 @@
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
+let length = 16;                               // Number
+let lastName = "Johnson";                      // String
+let x = {firstName:"John", lastName:"Doe"};    // Object
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+var thing = person.lastName;
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+const car1 = new Car('Eagle', 'Talon TSi', 1993);
